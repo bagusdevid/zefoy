1cb2ba371061c0df464f06bb3096652392e27710
TB Gita Priadi <66976191+bagusdevid@users.noreply.github.com>
2022-07-25 10:38:48 +0700

    Add files via upload

---
+++ baru.bat
@@ -0,0 +1,24 @@
+@echo off
+color a 
+
+echo INSTALLING...
+
+pip install -q requests
+pip install -q selenium
+pip install -q undetected-chromedriver
+pip install -q pystyle
+pip install discord.p
+pip install time 
+pip install calendar 
+pip install traceback
+pip install threading 
+pip install pysocks
+pip install random
+pip install json
+pip install tracemalloc 
+pip install asyncio 
+
+echo
+echo
+
+pause
